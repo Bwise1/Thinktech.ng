73e01323516768b3ecf5657cddbc7607a2fb1f54
Bwise1 <oguntoyebenjamin2.com>
2023-01-28 10:39:31 +0200

    Mail serverless function

---
+++ api/mail.go
@@ -0,0 +1,32 @@
+package handler
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+type Payload struct{
+	Email string
+	Name string
+	Message string
+
+}
+func Handler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/mail" {
+		http.Error(w, "404 not found.", http.StatusNotFound)
+		return
+	}
+	switch r.Method{
+	case "POST":
+		body, err := ioutil.ReadAll(r.Body)
+    if err != nil {
+        panic(err)
+    }
+    log.Println(string(body))
+	default:
+		fmt.Fprintf(w, "%v Method not allowed",r.Method)
+	}
+
+}
